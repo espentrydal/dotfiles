0e51a0eb29f0688e8520a737021ae7af6f0bad4e
Espen Trydal <espen@trydal.io>
2021-05-20 14:04:00 +0200

    configuring scala

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -244,3 +244,19 @@
         (goto-char (car ffap-string-at-point-region)))
 
     (ffap-next-url))))
+
+;; (defun set-exec-path-from-shell-PATH ()
+;;   "Set up Emacs' `exec-path' and PATH environment variable to match
+;; that used by the user's shell.
+
+;; This is particularly useful under Mac OS X and macOS, where GUI
+;; apps are not started from a shell."
+;;   (interactive)
+;;   (let ((path-from-shell (replace-regexp-in-string
+;;                           "[ \t\n]*$" "" (shell-command-to-string
+;;                                           "$SHELL --login -c 'echo $PATH'"
+;;                                           ))))
+;;     (setenv "PATH" path-from-shell)
+;;     (setq exec-path (split-string path-from-shell path-separator))))
+
+;; (set-exec-path-from-shell-PATH)
